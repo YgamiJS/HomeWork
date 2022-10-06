932f2234f61a5c68bd1522ad5ccea688721d85c7
Dima <dimasakharov@icloud.com>
2022-10-06 20:00:04 +0300

    6 commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,18 @@ import Content from './Components/Content.jsx'
 import styled from './App.module.scss'
 
 export default function App() {
-  
-  const [stateTime , setStateTime] = useState(new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes() )
-  setInterval( () => setStateTime(new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes()) , 60000)
+
+  let [date , setDate] = useState(new Date().toLocaleTimeString());
+
+  setInterval( () =>  setDate(new Date().toLocaleTimeString()) , 1000);
+
+
 
   return (
     <>
       <Header />
       <div className={styled.content}>
-        <h2 className="hours">{stateTime}</h2>
+        <h2 className="hours">{date}</h2>
         <Content props={{day: "понедельник" ,arrayLessons: [{ lessonInfo: {name: "Иностранный язык " ,  homeWork: "" }} , { lessonInfo: {name: "Информатика" ,  homeWork: "" }} , { lessonInfo: {name: "Русский язык" ,  homeWork: "" }} ,{ lessonInfo: {name: "Биология" ,  homeWork: "" }} ,{ lessonInfo: {name: "Математика" ,  homeWork: "" }} ,{ lessonInfo: {name: "Физическая культура" ,  homeWork: "" }} , { lessonInfo: {name: "" ,  homeWork: "" }}]}} />
         <Content props={{day: "вторник" ,arrayLessons: [{ lessonInfo: {name: "Физика" ,  homeWork: "" }} , { lessonInfo: {name: "ИЗО" ,  homeWork: "" }} , { lessonInfo: {name: "Русский язык" ,  homeWork: "" }} ,{ lessonInfo: {name: "История" ,  homeWork: "" }} ,{ lessonInfo: {name: "Литература" ,  homeWork: "" }} ,{ lessonInfo: {name: "Математика" ,  homeWork: "" }} , { lessonInfo: {name: "Музыка" ,  homeWork: "" }}]}} />
         <Content props={{day: "среда" ,arrayLessons: [{ lessonInfo: {name: "Технология" ,  homeWork: "" }} , { lessonInfo: {name: "Технология" ,  homeWork: "" }} , { lessonInfo: {name: "Литература" ,  homeWork: "" }} ,{ lessonInfo: {name: "Иностранный язык" ,  homeWork: "" }} ,{ lessonInfo: {name: "Математика" ,  homeWork: "" }} ,{ lessonInfo: {name: "ОНДКР" ,  homeWork: "" }} , { lessonInfo: {name: "История" ,  homeWork: "" }}]}} />
