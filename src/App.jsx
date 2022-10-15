7e02bacc2be0e0db77f37d4bb7d3a947d15ae0b9
Дмитрий <105663726+YgamiJS@users.noreply.github.com>
2022-10-15 22:49:13 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import { Header , Footer } from './Components/HeaderFooter.jsx'
 import Content from './Components/Content.jsx'
-import Disqus from "./Components/disqus.jsx"
 
 export default function App() {
   return (
     <>
       <Header />
       <Content />
-      <Disqus />
+      <div id="disqus_thread"></div>
+<script>
+    /**
+    *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+    *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+    /*
+    var disqus_config = function () {
+    this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    };
+    */
+    (function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://disqus-rhi7dnred7.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
       <Footer />
     </>
   )
