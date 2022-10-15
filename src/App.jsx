588a916c0bd065cd1eaa6e64d3033eab8ca6ef8a
Дмитрий <105663726+YgamiJS@users.noreply.github.com>
2022-10-15 22:23:17 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Header , Footer } from './Components/HeaderFooter.jsx'
 import Content from './Components/Content.jsx'
+import Disqus from "./Components/disqus.jsx"
 
 export default function App() {
   return (
     <>
       <Header />
       <Content />
+      <Disqus />
       <Footer />
     </>
   )
