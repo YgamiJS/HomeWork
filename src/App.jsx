d53b9786178b02dd5aa1ac49bb2e822cccbfb73b
Дмитрий <105663726+YgamiJS@users.noreply.github.com>
2022-10-16 11:11:09 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { Header , Footer } from './Components/HeaderFooter.jsx'
 import Content from './Components/Content.jsx'
-import Disqus from "./Components/Disqus.jsx"
 
 export default function App() {
   return (
     <>
       <Header />
       <Content />
-      <Disqus />
+      <div id="disqus_thread"></div>
+    {
+    (function() {
+    var d = document, s = d.createElement('script');
+    s.src = 'https://disqus-rhi7dnred7.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })()
+  }
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
       <Footer />
     </>
   )
