b76b183b2f955edb4b7e348a02231d35442818d7
Дмитрий <105663726+YgamiJS@users.noreply.github.com>
2022-10-16 09:20:04 +0300

    Update Disqus.jsx

--- src/Components/Disqus.jsx
+++ src/Components/Disqus.jsx
@@ -2,14 +2,14 @@ export default function Disqus(){
   return (
     <>
      <div id="disqus_thread"></div>
-<script>
+    {
     (function() {
     var d = document, s = d.createElement('script');
     s.src = 'https://disqus-rhi7dnred7.disqus.com/embed.js';
     s.setAttribute('data-timestamp', +new Date());
     (d.head || d.body).appendChild(s);
     })();
-</script>
+}
 <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
     </>
   )
